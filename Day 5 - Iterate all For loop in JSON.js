e74b95deeb7673b622cfdb27dfdb46c9d6de3f68
Prakash L <121917226+prakashlohanathan@users.noreply.github.com>
2023-06-21 13:21:58 +0530

    Add files via upload

---
+++ Day 5 - Iterate all For loop in JSON.js
@@ -0,0 +1,32 @@
+// for loop 
+
+// let numbers = [10,20,30,40,50,60]
+// for (i=0; i<numbers.length; i++) {
+//     numbers[i] = numbers[i] * 5;
+//     console.log(numbers[i]);
+// }
+
+// for in loop
+// let person = {
+//     name: 'chandresh',
+//     nickname: 'chandy',
+//     book: 'science fiction',
+//     hobby: 'cricket',
+//     bookprice: 550,
+//     };
+    // for (let key in person){
+    //     console.log(key, ':',person[key]);
+    // }
+
+// for of loop
+
+    // for (let key of Object.keys(person)) {
+    //     console.log(person[key]);
+    // }
+
+    // for Each of loop
+//  let array = [1, 2, 3, 4, 5];
+
+//  array.forEach((number, index, array) => {
+//         console.log(number, index, array[index]); 
+//  });
---
+++ Day 5 - Resume.js
@@ -0,0 +1,55 @@
+let myResume={
+    "basics": {
+      "name": "L.Prakash",
+      "email": "prakash2k2mech@gamil.com",
+      "phone": 9944887766,
+      "degree": "B.E., M.B.A",
+      "location": {
+        "address": "405,T v k Nagar",
+        "postalCode": 636004,
+        "city": "Salem",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      
+    "work": [
+      {
+        "company": "TVS BRAKES INDIA PVT LTD",
+        "position": "Asst-Manager- Business Development",
+        "startDate": "2013-05-31",
+        "endDate": "2015-12-31",
+        "summary": "i am the business executive handling various task and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Kongu Engneering College",
+        "department": "M.B.A",
+        "studyType": "fulltime",
+        "batch start year": 2011,
+        "batch end year": 2013,
+        "gpa": 7.67,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript,React",
+        "level": "beginer",
+        "project": [
+          "Created Blog"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Reading Books,Terrace Gardening,",
+      }
+    ]
+  }
+}
+  console.log(myResume);
